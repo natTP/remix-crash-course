8cbd9a6d28414af5e36a05c1f78ebb8c914ad467
Nattamon Polvichai <joppy.pgh@gmail.com>
2022-01-26 16:19:08 +0700

    ✨ create and delete posts

--- app/routes/posts/new.jsx
+++ app/routes/posts/new.jsx
@@ -1,4 +1,5 @@
 import { Link, redirect } from "remix";
+import { db } from "~/utils/db.server";
 
 export const action = async ({ request }) => {
   const form = await request.formData();
@@ -6,10 +7,9 @@ export const action = async ({ request }) => {
   const body = form.get("body");
 
   const fields = { title, body };
+  const post = await db.post.create({ data: fields });
 
-  //TODO : submit to database
-
-  return redirect("/posts");
+  return redirect(`/posts/${post.id}`);
 };
 
 function NewPost() {
